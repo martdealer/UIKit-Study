9de2a42c4b036a22523bfd4baa404696758ad124
martdealer <ios.martdealer@gmail.com>
2023-11-17 20:33:52 +0900

    refactor : MVC 패턴으로 변형

--- TableView/TableView/Controllers/ViewController.swift
+++ TableView/TableView/Controllers/ViewController.swift
@@ -7,39 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController {
     
-    var moviesArray: [Movie] = [
-        Movie(movieImage: UIImage(named: "batman"), movieName: "배트맨", movieDescription: "배트맨이 출연하는 영화")
+    var movieDataManager = DataManager()
     
-    
-    
-    
-    ]
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
-        cell.mainImageView.image = moviesArray[indexPath.row].movieImage
-        cell.movieNameLabel.text = moviesArray[indexPath.row].movieName
-        cell.descriptionLabel.text = moviesArray[indexPath.row].movieDescription
-        
-        return cell
-    }
-    
-
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.dataSource = self
         tableView.rowHeight = 120
+        
+        movieDataManager.makeMovieData()
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieDataManager.getMovieData().count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movieDataManager.getMovieData()[indexPath.row]
+        cell.mainImageView.image = movie.movieImage
+        cell.movieNameLabel.text = movie.movieName
+        cell.descriptionLabel.text = movie.movieDescription
+        
+        return cell
+        
+    }
+}
