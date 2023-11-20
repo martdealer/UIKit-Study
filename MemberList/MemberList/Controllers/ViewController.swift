1ddabb9ad7f23c30aab9d6e6520b6020bc19e93f
martdealer <ios.martdealer@gmail.com>
2023-11-20 17:51:20 +0900

    feature : 테이블뷰, 내비게이션바 구현 + 모델링

---
+++ MemberList/MemberList/Controllers/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by zsm on 11/20/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableViewConstraints()
+        tableView.dataSource = self
+    }
+    
+    // 테이블뷰의 오토레이아웃 설정
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
---
+++ MemberList/MemberList/Models/Member.swift
@@ -0,0 +1,38 @@
+//
+//  Member.swift
+//  MemberList
+//
+//  Created by zsm on 11/20/23.
+//
+
+import UIKit
+
+struct Member {
+    
+    // 이미지가 없다면 메모리 낭비하지 않도록 lazy 선언
+    lazy var memberImage: UIImage? = {
+        guard let name = name else { return UIImage(systemName: "person") }
+        return UIImage(named: "\(name).png") ?? UIImage(systemName: "person")
+    }()
+    
+    // 총 멤버수. 모든 인스턴스가 같은 값을 가지고 공유해야 하므로 타입변수static 선언
+    static var memberNumber: Int = 0
+    
+    let memberId: Int
+    var name: String?
+    var age: Int?
+    var phone: String?
+    var address: String?
+    
+    init(name: String?, age: Int?, phone: String?, address: String?) {
+        
+        self.memberId = Member.memberNumber == 0 ? 0 : Member.memberNumber
+        
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        
+        Member.memberNumber += 1
+    }
+}
