5a156b71853ca37676ff75e88ea6b9e24d900ac8
martdealer <ios.martdealer@gmail.com>
2023-11-27 10:02:21 +0900

    initial commit

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by zsm on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
